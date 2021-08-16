3e3f4196c15b2e18d7dde4147ba7673eb11c4d70
Larissa Carvalho <larissa.scsantos6@gmail.com>
2021-08-16 14:09:29 -0300

    mudando o ex001.py

--- scripts/ex001.py
+++ scripts/ex001.py
@@ -1,3 +1,9 @@
 # Desafio 001: Crie um  programa que escreva "Olá, Mundo!".
 
 print("Olá, Mundo!")
+
+# Podemos também executar o mesmo programa atribuindo uma string a uma variável e printar esta.
+
+msg = "Olá, Mundo!"
+
+print(msg)
